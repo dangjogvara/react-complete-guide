17e767ae850c46558a3dcc6150925b27a4055a65
Dan Poulsen <dangjogvara@gmail.com>
2021-11-20 23:19:43 +0000

    replace hardcoded content

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
-  const expenseItem = new Date(2021, 11, 20).toUTCString();
+  const expenseItem = new Date(2021, 11, 20);
   const expenseTitle = 'Car Insurance';
   const expenseAmount = 295.67;
 
   return (
     <div className="expense-item">
-      <div>{expenseItem}</div>
+      <div>{expenseItem.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">{expenseAmount}</div>
