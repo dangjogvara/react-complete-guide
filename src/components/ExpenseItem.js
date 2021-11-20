1f53fa05f3550b2483a7fc8c37e67b07c04c6eb2
Dan Poulsen <dangjogvara@gmail.com>
2021-11-20 11:39:17 +0000

    add variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseItem = new Date(2021, 11, 20).toUTCString();
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 295.67;
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseItem}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
