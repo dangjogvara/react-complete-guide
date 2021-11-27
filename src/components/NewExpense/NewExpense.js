b425fe0138125061ab76d3a2e45eaaa3d30e4ad2
Dan Poulsen <dangjogvara@gmail.com>
2021-11-27 23:10:11 +0000

    NewExpense and Form components

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,5 +1,14 @@
-function NewExpense() {
-  return <div></div>;
-}
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
 
 export default NewExpense;
