b829fe9d43351f289816df831fa574127cc080e6
Dan Poulsen <dangjogvara@gmail.com>
2021-11-28 00:40:12 +0000

    formatting

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,11 +4,11 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = () => {
-  return (
-    <div className="new-expense">
-      <ExpenseForm />
-    </div>
-  );
+    return (
+        <div className="new-expense">
+            <ExpenseForm />
+        </div>
+    );
 };
 
 export default NewExpense;
